196a4bb1e51c584f840e83c552d8f428bdf531a2
Vytis Valentinavičius <vytis@nolife4life.org>
2023-12-14 21:49:12 +0200

    day 12 (yay)

--- lib/aoc2023/d12.ex
+++ lib/aoc2023/d12.ex
@@ -14,50 +14,125 @@ defmodule AOC2023.D12 do
     File.read!("input/d12.txt")
   end
 
-  def parse(input) do
+  def parse(input, amount \\ 1) do
     input
     |> String.split("\n", trim: true)
-    |> Enum.map(fn line ->
-      [springs, pools] = String.split(line, " ", trim: true, parts: 2)
-
-      springs =
-        String.split(springs, "", trim: true)
-        |> Enum.map(fn spring ->
-          case spring do
-            "." -> :operational
-            "#" -> :damaged
-            "?" -> :unknown
-          end
-        end)
-
-      pools =
-        String.split(pools, ",", trim: true)
-        |> Enum.map(&String.to_integer/1)
-
-      {springs, pools}
-    end)
+    |> Enum.map(&parse_line(&1, amount))
   end
 
-  def permutations(pools, length) do
-    # pool, 0, pool, 0, pool
+  def parse_line(line, amount) do
+    line
+    |> String.split(" ", trim: true, parts: 2)
+    |> unfold(amount)
+    |> parse_springs_pools()
   end
 
-  def is_valid_permutation?(permutation, spring) do
-    Enum.zip(permutation, spring)
-    |> Enum.all?(fn {p, s} ->
-      case s do
-        :unknown -> true
-        :damaged -> p == :damaged
-        :operational -> p == :operational
+  def parse_springs_pools([springs, pools]) do
+    springs =
+      String.split(springs, "", trim: true)
+
+    pools =
+      String.split(pools, ",", trim: true)
+      |> Enum.map(&String.to_integer/1)
+
+    {springs, pools}
+  end
+
+  def unfold([springs, pools], amount) do
+    springs = for _ <- 1..amount, do: springs
+    pools = for _ <- 1..amount, do: pools
+
+    [springs |> Enum.join("?"), pools |> Enum.join(",")]
+  end
+
+  def is_valid?(springs, pools)
+
+  def is_valid?([], []), do: true
+  def is_valid?([], [{0}]), do: true
+  def is_valid?([], _), do: false
+
+  def is_valid?(["#" | _], [{0} | _]), do: false
+
+  # A failing pool starts with a untouched pool
+  def is_valid?(["#" | remaining_springs], [current | remaining_pools]) when is_number(current),
+    do: is_valid?(remaining_springs, [{current - 1} | remaining_pools])
+
+  def is_valid?(["#" | remaining_springs], [{current} | remaining_pools]),
+    do: is_valid?(remaining_springs, [{current - 1} | remaining_pools])
+
+  # Each failing pool must terminate with a spring
+  def is_valid?(["." | remaining_springs], [{0} | remaining_pools]),
+    do: is_valid?(remaining_springs, remaining_pools)
+
+  # You can not terminate non-completed pools with a spring
+  def is_valid?(["." | _], [{_} | _]), do: false
+
+  def is_valid?(["." | remaining_springs], pools), do: is_valid?(remaining_springs, pools)
+
+  def is_valid?(_, []), do: false
+
+  def count_valid(known, springs, pools)
+
+  def count_valid(known, [], []), do: {known, 1}
+  def count_valid(known, [], [{0}]), do: {known, 1}
+  def count_valid(known, [], _), do: {known, 0}
+
+  def count_valid(known, ["#" | _], [{0} | _]), do: {known, 0}
+
+  def count_valid(known, ["#" | remaining_springs], [current | remaining_pools])
+      when is_number(current),
+      do: count_valid(known, remaining_springs, [{current - 1} | remaining_pools])
+
+  def count_valid(known, ["#" | remaining_springs], [{current} | remaining_pools]),
+    do: count_valid(known, remaining_springs, [{current - 1} | remaining_pools])
+
+  def count_valid(known, ["." | remaining_springs], [{0} | remaining_pools]),
+    do: count_valid(known, remaining_springs, remaining_pools)
+
+  def count_valid(known, ["." | _], [{_} | _]), do: {known, 0}
+
+  def count_valid(known, ["." | remaining_springs], pools),
+    do: count_valid(known, remaining_springs, pools)
+
+  def count_valid(known, ["?" | remaining_springs], pools) do
+    {known, left} =
+      case Map.get(known, {["#" | remaining_springs], pools}) do
+        nil -> count_valid(known, ["#" | remaining_springs], pools)
+        c -> {known, c}
       end
-    end)
+
+    known = Map.put(known, {["#" | remaining_springs], pools}, left)
+
+    {known, right} =
+      case Map.get(known, {["." | remaining_springs], pools}) do
+        nil -> count_valid(known, ["." | remaining_springs], pools)
+        c -> {known, c}
+      end
+
+    known = Map.put(known, {["." | remaining_springs], pools}, right)
+
+    {known, left + right}
   end
+
+  def count_valid(known, _, []), do: {known, 0}
 
   def part1(input) do
-    1
+    input
+    |> parse()
+    |> Enum.map(fn {springs, pools} ->
+      {_, count} = count_valid(%{}, springs, pools)
+      count
+    end)
+    |> Enum.sum()
   end
 
   def part2(input) do
-    2
+    input
+    |> parse(5)
+    |> Enum.map(fn {springs, pools} ->
+      {_, count} = count_valid(%{}, springs, pools)
+      count
+    end)
+    |> Enum.sum()
   end
 end
--- test/aoc2023/d12_test.exs
+++ test/aoc2023/d12_test.exs
@@ -11,33 +11,37 @@ defmodule AOC2023.D12Test do
              ?###???????? 3,2,1
              """>>
 
-  @test_p2 <<"""
-             """>>
-
   test "part1 on test data" do
-    assert AOC2023.D12.part1(@test_p1) == 0
+    assert AOC2023.D12.part1(@test_p1) == 21
   end
 
   test "part2 on test data" do
-    assert AOC2023.D12.part2(@test_p2) == 0
+    assert AOC2023.D12.part2(@test_p1) == 525_152
   end
 
   test "internals" do
     assert AOC2023.D12.parse("???.### 1,1,3") == [
              {
-               [:unknown, :unknown, :unknown, :operational, :damaged, :damaged, :damaged],
+               ["?", "?", "?", ".", "#", "#", "#"],
                [1, 1, 3]
              }
            ]
 
-    assert AOC2023.D12.is_valid_permutation?(
-             [:operational, :damaged, :operational, :operational, :damaged, :damaged, :damaged],
-             [:unknown, :unknown, :unknown, :operational, :damaged, :damaged, :damaged]
-           )
+    assert AOC2023.D12.is_valid?(["#", ".", "#", ".", "#", "#", "#"], [1, 1, 3]) == true
+    assert AOC2023.D12.is_valid?(["#", "#", "#", ".", "#", "#", "#"], [1, 1, 3]) == false
+    assert AOC2023.D12.is_valid?(["#", "#", ".", ".", "#", "#", "#"], [1, 1, 3]) == false
+    assert AOC2023.D12.is_valid?([".", ".", ".", ".", "#", "#", "#"], [1, 1, 3]) == false
+    assert AOC2023.D12.is_valid?([".", ".", ".", ".", "#", "#", "#"], [3, 1, 1]) == false
+    assert AOC2023.D12.is_valid?(["#", ".", "#", ".", "#", ".", "."], [3]) == false
 
-    assert !AOC2023.D12.is_valid_permutation?(
-             [:operational, :damaged, :operational, :damaged, :damaged, :damaged, :damaged],
-             [:unknown, :unknown, :unknown, :operational, :damaged, :damaged, :damaged]
-           )
+    assert AOC2023.D12.count_valid(["#", ".", "#", ".", "#", "#", "#"], [1, 1, 3]) == 1
+    assert AOC2023.D12.count_valid(["#", "#", "#", ".", "#", "#", "#"], [1, 1, 3]) == 0
+    assert AOC2023.D12.count_valid(["#", "#", ".", ".", "#", "#", "#"], [1, 1, 3]) == 0
+    assert AOC2023.D12.count_valid([".", ".", ".", ".", "#", "#", "#"], [1, 1, 3]) == 0
+    assert AOC2023.D12.count_valid([".", ".", ".", ".", "#", "#", "#"], [3, 1, 1]) == 0
+    assert AOC2023.D12.count_valid(["#", ".", "#", ".", "#", ".", "."], [3]) == 0
+    assert AOC2023.D12.count_valid(["?", "?", "?", ".", "#", "#", "#"], [1, 1, 3]) == 1
+
+    assert AOC2023.D12.count_valid(["?", "?", ".", "?", "?", ".", "#"], [1, 1, 1]) == 4
   end
 end
